d56c8b2e2c6613e109cf599d15d9242cfbd71707
vedparsekar <vedparsekar@gmail.com>
2020-11-19 17:39:56 +0530

    added pin UI

--- app/src/main/java/com/example/womensafetyapp/MainActivity.java
+++ app/src/main/java/com/example/womensafetyapp/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.womensafetyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button sos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sos = findViewById(R.id.sos_btn);
+        sos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Pin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/womensafetyapp/Pin.java
@@ -0,0 +1,17 @@
+package com.example.womensafetyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Toast;
+
+
+public class Pin extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin);
+    }
+}
