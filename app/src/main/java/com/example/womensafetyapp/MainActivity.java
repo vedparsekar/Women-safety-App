92255d05896d79289058bd26a0c4dc03269e9b50
vedparsekar <vedparsekar@gmail.com>
2020-11-21 20:36:05 +0530

    Profile UI

--- app/src/main/java/com/example/womensafetyapp/MainActivity.java
+++ app/src/main/java/com/example/womensafetyapp/MainActivity.java
@@ -1,26 +1,45 @@
 package com.example.womensafetyapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
     Button sos;
+    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sos = findViewById(R.id.sos_btn);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
         sos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Pin.class);
                 startActivity(intent);
+            }
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.page_4) {
+                    Intent intent = new Intent(getApplicationContext(), Profile.class);
+                    startActivity(intent);
+                }
+                return false;
             }
         });
     }
